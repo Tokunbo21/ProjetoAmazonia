0265f420362214a0482744ef0c01ac59c8b7aece
unknown <tofadetokunbo@hotmail.com>
2021-11-04 18:34:34 -0300

    Terceira pagina

---
+++ script.js
@@ -0,0 +1,31 @@
+/*Index.html */
+//alert("Página ainda em Obra")
+/*var vOuF = false;
+console.log(vOuF) console.log(typof()) mostra qual é o tipo da variavel*/   
+
+let array = ['string', 1, true, false, ['array1'],['array2'],['array3']];
+//console.log(array);
+
+//forEach
+//array.forEach(function(item, index){console.log(item, index)});
+
+/*push
+array.push('novo item',)
+console.log(array) */
+
+/*array.pop();
+console.log(array);*/
+
+/*array.shift();
+console.log(array);*/
+
+/*array.unshift('novo intem aqui');
+console.log(array)*/
+
+//console.log(array.indexOf(false));
+
+/*array.splice(0, 3);
+console.log(array);*/
+
+/*let novoArray = array.slice(0,4);
+console.log(novoArray);*/
