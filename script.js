becda98d2d8cdad9e473ca513189bad509665250
Tokunbo Tofade <tofadetokunbo@hotmail.com>
2021-11-12 17:50:59 -0300

    Página Sobre

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /*Index.html */
 //alert("Página ainda em Obra")
 /*var vOuF = false;
-console.log(vOuF) console.log(typof()) mostra qual é o tipo da variavel*/   
+console.log(vOuF) console.log(typof()) mostra qual é o tipo da variavel*/
 
-let array = ['string', 1, true, false, ['array1'],['array2'],['array3']];
+//let array = ['string', 1, true, false, ['array1'], ['array2'], ['array3']];
 //console.log(array);
 
 //forEach
@@ -29,3 +29,78 @@ console.log(array);*/
 
 /*let novoArray = array.slice(0,4);
 console.log(novoArray);*/
+
+/*var jogador1 = 0;
+var jogador2 = 0;
+var placar;*/
+
+/*if(jogador1 != -1){
+    if(jogador1 > 0){
+    console.log("jogador1 marcou ponto!");
+} else if(jogador2 > 0){
+  console.log("jogadr2 marcou um ponto")
+  }else {
+  console.log("Ninguém marcou")
+}
+}*/
+
+/*if ternario
+jogador1 != -1 && jogador2 != -1 ? console.log('Os jogadores são validos') :
+    console.log('jogador Invalido')
+if (jogador1 > 0 && jogador2 == 0) {
+    console.log("jogador1 marcou ponto!");
+    placar = jogador1 > jogador2;
+
+} else if (jogador2 > 0 && jogador1 == 0) {
+    console.log("jogadr2 marcou um ponto")
+    placar = jogador2 > jogador1;
+} else {
+    console.log("Ninguém marcou")
+}
+
+switch (placar) {
+    case placar = jogador1 > jogador2:
+        console.log('Jogador1 ganhou');
+        break;
+    case placar = jogador2 > jogador1:
+        console.log('Jogador2 ganhou');
+        break;
+        default:
+            console.log('O jogo continua empatado')
+        
+}*/
+
+/*let array = ['valor1','valor2','valor3','valor4','valor5',];
+
+let object = {propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3',}*/
+
+/*for (let indice = 0; indice< array.length; indice++) {
+    console.log(indice);
+}
+
+for(let i in array) {
+    console.log(i)
+}
+
+for(i in object){
+    console.log(i)
+}*/
+
+/*for(let i of array) {
+    console.log(i)
+}*/
+
+//Function declarativa
+// function tokunbo(){
+//     console.log("Olá Tokunbo")
+// }
+// tokunbo();
+
+// var funcao = function (){
+//     console.log("Uma mensagem de uma função")
+// }
+
+// funcao();
+
+
+
